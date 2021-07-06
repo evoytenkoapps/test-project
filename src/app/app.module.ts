e7f9eba992a7f70d67c2993c77862731a889242c
Войтенко Евгений Игоревич <eivoyten@mts.ru>
2021-07-06 18:04:48 +0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
+import { GraphModule, NgxGraphModule } from '@swimlane/ngx-graph';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, MainComponent],
+  imports: [BrowserModule, NgxGraphModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
