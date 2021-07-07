0aaca4eb613b8ed216212499d0d033e1de0547a5
Войтенко Евгений Игоревич <eivoyten@mts.ru>
2021-07-07 08:37:46 +0300

    add tree

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
 import { GraphModule, NgxGraphModule } from '@swimlane/ngx-graph';
+import { NgxGraphOrgTreeComponent } from './components/ngx-graph-org-tree/ngx-graph-org-tree.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent, MainComponent],
-  imports: [BrowserModule, NgxGraphModule],
+  declarations: [AppComponent, MainComponent, NgxGraphOrgTreeComponent],
+  imports: [BrowserModule, NgxGraphModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
