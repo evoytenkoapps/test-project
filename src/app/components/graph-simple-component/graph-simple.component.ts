8be414d21f5f5b6f1ff1927d65d7aa508e3deaa2
Войтенко Евгений Игоревич <eivoyten@mts.ru>
2021-07-09 11:32:18 +0300

    add echart

--- src/app/components/graph-simple-component/graph-simple.component.ts
+++ src/app/components/graph-simple-component/graph-simple.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import * as echarts from 'echarts/core';
+import { EChartsOption, GraphSeriesOption } from 'echarts';
+
+interface NodeData {
+  name: string;
+  x: number;
+  y: number;
+}
+
+interface LinkData {
+  source: string;
+  target: string;
+}
 
 @Component({
   selector: 'app-graph-simple-component',
   templateUrl: './graph-simple.component.html',
   styleUrls: ['./graph-simple.component.css'],
 })
-export class GraphSimpleComponent {
+export class GraphSimpleComponent implements OnInit {
   options = {
     title: {
       text: 'Simple Graph',
@@ -17,7 +30,7 @@ export class GraphSimpleComponent {
       {
         type: 'graph',
         layout: 'none',
-        symbolSize: 60,
+        symbolSize: 20,
         roam: true,
         label: {
           normal: {
@@ -29,79 +42,79 @@ export class GraphSimpleComponent {
         edgeLabel: {
           normal: {
             textStyle: {
-              fontSize: 20,
+              fontSize: 10,
             },
           },
         },
         data: [
-          {
-            name: 'Node 1',
-            x: 300,
-            y: 300,
-          },
-          {
-            name: 'Node 2',
-            x: 800,
-            y: 300,
-          },
-          {
-            name: 'Node 3',
-            x: 550,
-            y: 100,
-          },
-          {
-            name: 'Node 4',
-            x: 550,
-            y: 500,
-          },
+          // {
+          //   name: 'Node 1',
+          //   x: 0,
+          //   y: 0,
+          // },
+          // {
+          //   name: 'Node 2',
+          //   x: 0,
+          //   y: 0,
+          // },
+          // {
+          //   name: 'Node 3',
+          //   x: 550,
+          //   y: 100,
+          // },
+          // {
+          //   name: 'Node 4',
+          //   x: 550,
+          //   y: 500,
+          // },
         ],
-        // links: [],
-        links: [
-          {
-            source: 0,
-            target: 1,
-            symbolSize: [5, 20],
-            label: {
-              normal: {
-                show: true,
-              },
-            },
-            lineStyle: {
-              normal: {
-                width: 5,
-                curveness: 0.2,
-              },
-            },
-          },
-          {
-            source: 'Node 2',
-            target: 'Node 1',
-            label: {
-              normal: {
-                show: true,
-              },
-            },
-            lineStyle: {
-              normal: { curveness: 0.2 },
-            },
-          },
-          {
-            source: 'Node 1',
-            target: 'Node 3',
-          },
-          {
-            source: 'Node 2',
-            target: 'Node 3',
-          },
-          {
-            source: 'Node 2',
-            target: 'Node 4',
-          },
-          {
-            source: 'Node 1',
-            target: 'Node 4',
-          },
-        ],
+        links: [],
+        // links: [
+        //   {
+        //     source: 0,
+        //     target: 1,
+        //     symbolSize: [5, 20],
+        //     label: {
+        //       normal: {
+        //         show: true,
+        //       },
+        //     },
+        //     lineStyle: {
+        //       normal: {
+        //         width: 5,
+        //         curveness: 0.2,
+        //       },
+        //     },
+        //   },
+        //   {
+        //     source: 'Node 2',
+        //     target: 'Node 1',
+        //     label: {
+        //       normal: {
+        //         show: true,
+        //       },
+        //     },
+        //     lineStyle: {
+        //       normal: { curveness: 0.2 },
+        //     },
+        //   },
+        //   {
+        //     source: 'Node 1',
+        //     target: 'Node 3',
+        //   },
+        //   {
+        //     source: 'Node 2',
+        //     target: 'Node 3',
+        //   },
+        //   {
+        //     source: 'Node 2',
+        //     target: 'Node 4',
+        //   },
+        //   {
+        //     source: 'Node 1',
+        //     target: 'Node 4',
+        //   },
+        // ],
         lineStyle: {
           normal: {
             opacity: 0.9,
@@ -112,4 +125,22 @@ export class GraphSimpleComponent {
       },
     ],
   };
+
+  ngOnInit(): void {
+    const data = this.options.series[0].data as NodeData[];
+    const links = this.options.series[0].links as LinkData[];
+    for (let i = 1; i <= 5; i++) {
+      if (i === 1) {
+        data.push({ name: 'Node ' + i, x: 100, y: i * 100 });
+        continue;
+      }
+      const name = 'Node ' + i;
+      const nodeData: NodeData = { name, x: 200, y: i * 100 };
+      data.push(nodeData);
+      links.push({
+        source: 'Node 1',
+        target: name,
+      });
+    }
+  }
 }
