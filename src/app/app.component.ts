c7d93ed1b4037f03db29f526e840cf8a4f2dd414
Войтенко Евгений Игоревич <eivoyten@mts.ru>
2021-08-06 13:36:01 +0300

    start oauth2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,9 +11,16 @@ export class AppComponent {
   title = 'test-project';
 
   constructor(private oAuthService: OAuthService) {
-    this.oAuthService.initCodeFlow();
+    // this.oAuthService.initCodeFlow();
     this.oAuthService.configure(authCodeFlowConfig);
-    this.oAuthService.loadDiscoveryDocumentAndTryLogin();
+    // this.oAuthService.loadDiscoveryDocumentAndTryLogin();
+    this.oAuthService.loadDiscoveryDocumentAndTryLogin().then((_) => {
+      if (!this.oAuthService.hasValidAccessToken()) {
+        this.oAuthService.initCodeFlow();
+      } else {
+        this.oAuthService.setupAutomaticSilentRefresh();
+      }
+    });
 
     console.log('token', this.oAuthService.getAccessToken());
   }
