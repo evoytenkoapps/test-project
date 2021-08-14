1964dd3f6e900bfb552f37f5f5f0848707347bb2
Войтенко Евгений Игоревич <eivoyten@mts.ru>
2021-08-14 11:50:24 +0300

    init

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
 })
 export class MainComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  public getText(): string {
+    console.log('function');
+    return 'function';
   }
 
+  get text(): string {
+    console.log('getter');
+    return 'getter';
+  }
 }
